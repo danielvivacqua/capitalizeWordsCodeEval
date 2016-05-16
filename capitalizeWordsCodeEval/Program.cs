05e4cdec103e29ec3005f61cdc78fab7b2945e6f
Your Name <you@example.com>
2016-05-16 11:46:55 -0400

    A CodeEval challenge

---
+++ capitalizeWordsCodeEval/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capitalizeWordsCodeEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = {
+                                "Hello world",
+                                "javaScript language",
+                                "a letter",
+                                "1st thing"
+                             };
+            foreach (string line in lines)
+            {
+                string[] sentence = line.Split(' ');
+                int i, sentLen = sentence.Length;
+                for (i = 0; i < sentLen; i++)
+                {
+                    Console.Write(TitleCase(sentence[i]));
+                    if (i < sentLen - 1)
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+        static string TitleCase(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            char[] upCase = s.ToCharArray();
+            upCase[0] = char.ToUpper(upCase[0]);
+            return new string(upCase);
+        }
+    }
+}
